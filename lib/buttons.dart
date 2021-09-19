b2c0c9ed358baf64fb608a18d13f734755d75c64
Brendan DeLancey <bdelancey98@gmail.com>
2021-09-19 01:51:01 -0400

    Button padding issue, as well as text size and position issues fixed

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 
 class Buttons extends StatelessWidget {
-  final DatabaseReference database = FirebaseDatabase.instance.reference().child("test");
+  final DatabaseReference database =
+      FirebaseDatabase.instance.reference().child("test");
 
 //  sendData(){
 ////    database.push().set({'name':'Peter','lastName':'Ross'
@@ -22,10 +23,12 @@ class Buttons extends StatelessWidget {
       TableRow(children: <Widget>[
         Container(
 //                width: ,
-          height: MediaQuery.of(context).size.height ,
+          height: MediaQuery.of(context).size.height,
 
-          child: Padding( padding:EdgeInsets.only( bottom: 100.0),
-          child:ElevatedButton(
+          // child:
+          // Padding(
+          // padding:EdgeInsets.only( bottom: 100.0),
+          child: ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/second');
               },
@@ -34,33 +37,63 @@ class Buttons extends StatelessWidget {
 
               // child: Text('Liberal', , style: TextStyle(color: Colors.white,fontSize: 28,fontWeight:FontWeight.bold,fontStyle:FontStyle.italic,  ), ),
               // color: Color(0xFFD50000)),
-              child: Text('Liberal', textAlign: TextAlign.right,style: TextStyle(color: Colors.white,fontSize: 28,fontWeight:FontWeight.bold,fontStyle:FontStyle.italic  ),),
+              child: Align(
+                  alignment: Alignment(0, -0.2),
+                  child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Text(
+                        'Liberal',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                            // height: 20,
+                            color: Colors.white,
+                            fontSize: 29,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic),
+                      ))),
               style: ElevatedButton.styleFrom(
-                  primary: Color(0xFFD50000),
-                  shape: const BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(0))),
+                primary: Color(0xFFD50000),
+                onPrimary: Color(0x7D0404),
+                shape: const BeveledRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(0))),
+              )),
 
-    )),
-
-          )),
+          // )
+        ),
         Container(
-          height: MediaQuery.of(context).size.height ,
+          height: MediaQuery.of(context).size.height,
 
-          child: Padding( padding:EdgeInsets.only( bottom: 100.0),
-              child:ElevatedButton(
+          // child: Padding(
+          // padding:EdgeInsets.only( bottom: 0),
+          child: ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/third');
                 //sendData();
               },
 //              check for relative sizing for larger phones and the logos being positioned poorly
 
-              child: Text('Conservative', style: TextStyle(color: Colors.white,fontSize: 28,fontWeight:FontWeight.bold,fontStyle:FontStyle.italic  ),),
+              child: Align(
+                  alignment: Alignment(0, -0.2),
+                  child: FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Text(
+                        'Conservative',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 29,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic),
+                      ))),
               style: ElevatedButton.styleFrom(
                 // shape: BorderRadius.all(0),
-                  primary: Color(0xFF01579B),
-                  shape: const BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(0))),
 
+                primary: Color(0xFF01579B),
+                onPrimary: Color(0x4147D),
+                shape: const BeveledRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(0))),
               )),
-        )),
+          // )
+        ),
       ]),
     ]);
   }
